0bf94f7cd821b305e2a2bef5a78aed8ed6c6b717
SSARCandy <ssarcandy@gmail.com>
2019-10-07 02:00:27 +0800

    :recycle: refactor ETF class

--- include/ETF.h
+++ include/ETF.h
@@ -10,19 +10,18 @@ public:
     ETF(cv::Size);
     void initial_ETF(std::string, cv::Size);
     void refine_ETF(int kernel);
-    void rotateFlow(cv::Mat &src, cv::Mat &dst, float theta);
 
     cv::Mat gradientMag; // Normalized gradient magnitude
     cv::Mat flowField;   // edge tangent flow
     cv::Mat refinedETF;  // ETF after refinement
 
 private:
-    void resizeMat(cv::Size);
-    void computeNewVector(int x, int y, const int kernel);
-    float computePhi(cv::Vec3f x, cv::Vec3f y);
-    float computeWs(cv::Point2f x, cv::Point2f y, int r);
-    float computeWm(float gradmag_x, float gradmag_y);
-    float computeWd(cv::Vec3f x, cv::Vec3f y);
+    cv::Mat rotate(const cv::Mat &src, const double degree);
+    cv::Vec3f computeNewVector(const int x, const int y, const int kernel);
+    float computePhi(const cv::Vec3f &x, const cv::Vec3f &y);
+    float computeWs(const cv::Point2f &x, const cv::Point2f &y, const int r);
+    float computeWm(const float gradmag_x, const float gradmag_y);
+    float computeWd(const cv::Vec3f &x, const cv::Vec3f &y);
 
     cv::Size s;
 };
--- src/ETF.cpp
+++ src/ETF.cpp
@@ -27,7 +27,10 @@ ETF::ETF(cv::Size s)
  */
 void ETF::initial_ETF(std::string file, cv::Size s)
 {
-    resizeMat(s);
+    // Resizing Mat
+    cv::resize(flowField, flowField, s, 0, 0, CV_INTER_LINEAR);
+    cv::resize(refinedETF, refinedETF, s, 0, 0, CV_INTER_LINEAR);
+    cv::resize(gradientMag, gradientMag, s, 0, 0, CV_INTER_LINEAR);
 
     cv::Mat src = cv::imread(file, 1);
     cv::Mat src_n;
@@ -47,9 +50,9 @@ void ETF::initial_ETF(std::string file, cv::Size s)
     flowField = cv::Mat::zeros(src.size(), CV_32FC3);
 
 #pragma omp parallel for
-    for (int i = 0; i < src.rows; i++)
+    for (int i = 0; i < src.rows; ++i)
     {
-        for (int j = 0; j < src.cols; j++)
+        for (int j = 0; j < src.cols; ++j)
         {
             cv::Vec3f u = grad_x.at<cv::Vec3f>(i, j);
             cv::Vec3f v = grad_y.at<cv::Vec3f>(i, j);
@@ -58,92 +61,91 @@ void ETF::initial_ETF(std::string file, cv::Size s)
         }
     }
 
-    rotateFlow(flowField, flowField, 90);
+    flowField = rotate(flowField, 90);
 }
 
 
 void ETF::refine_ETF(int kernel)
 {
 #pragma omp parallel for
-    for (int r = 0; r < flowField.rows; r++)
+    for (int r = 0; r < flowField.rows; ++r)
     {
-        for (int c = 0; c < flowField.cols; c++)
+        for (int c = 0; c < flowField.cols; ++c)
         {
-            computeNewVector(c, r, kernel);
+            refinedETF.at<cv::Vec3f>(r, c) = computeNewVector(c, r, kernel);
         }
     }
 
     flowField = refinedETF.clone();
 }
 
+
+cv::Mat ETF::rotate(const cv::Mat &src, const double degree)
+{
+    const double theta{degree / 180.0 * M_PI};
+    cv::Mat dst{cv::Mat::zeros(src.size(), CV_32FC3)};
+
+    for (int i = 0; i < src.rows; ++i)
+    {
+        for (int j = 0; j < src.cols; ++j)
+        {
+            cv::Vec3f v    = src.at<cv::Vec3f>(i, j);
+            const float rx = v[0] * cos(theta) - v[1] * sin(theta);
+            const float ry = v[1] * cos(theta) + v[0] * sin(theta);
+
+            dst.at<cv::Vec3f>(i, j) = cv::Vec3f(rx, ry, 0.0);
+        }
+    }
+
+    return dst;
+}
+
 /*
  * Paper's Eq(1)
  */
-void ETF::computeNewVector(int x, int y, const int kernel)
+cv::Vec3f ETF::computeNewVector(const int x, const int y, const int kernel)
 {
     const cv::Vec3f t_cur_x = flowField.at<cv::Vec3f>(y, x);
     cv::Vec3f t_new         = cv::Vec3f(0, 0, 0);
 
-    for (int r = y - kernel; r <= y + kernel; r++)
+    for (int r = y - kernel; r <= y + kernel; ++r)
     {
-        for (int c = x - kernel; c <= x + kernel; c++)
+        for (int c = x - kernel; c <= x + kernel; ++c)
         {
             if (r < 0 || r >= refinedETF.rows || c < 0 || c >= refinedETF.cols) continue;
 
             const cv::Vec3f t_cur_y = flowField.at<cv::Vec3f>(r, c);
-            float phi               = computePhi(t_cur_x, t_cur_y);
-            float w_s               = computeWs(cv::Point2f(x, y), cv::Point2f(c, r), kernel);
-            float w_m = computeWm(norm(gradientMag.at<cv::Vec3f>(y, x)), cv::norm(gradientMag.at<float>(r, c)));
-            float w_d = computeWd(t_cur_x, t_cur_y);
+
+            const float phi = computePhi(t_cur_x, t_cur_y);
+            const float w_s = computeWs(cv::Point2f(x, y), cv::Point2f(c, r), kernel);
+            const float w_m = computeWm(norm(gradientMag.at<cv::Vec3f>(y, x)), cv::norm(gradientMag.at<float>(r, c)));
+            const float w_d = computeWd(t_cur_x, t_cur_y);
             t_new += phi * t_cur_y * w_s * w_m * w_d;
         }
     }
-    refinedETF.at<cv::Vec3f>(y, x) = cv::normalize(t_new);
+    return cv::normalize(t_new);
 }
 
 /*
  * Paper's Eq(5)
  */
-float ETF::computePhi(cv::Vec3f x, cv::Vec3f y) { return x.dot(y) > 0 ? 1 : -1; }
+float ETF::computePhi(const cv::Vec3f &x, const cv::Vec3f &y) { return x.dot(y) > 0 ? 1 : -1; }
 
 /*
  * Paper's Eq(2)
  */
-float ETF::computeWs(cv::Point2f x, cv::Point2f y, int r) { return norm(x - y) < r ? 1 : 0; }
+float ETF::computeWs(const cv::Point2f &x, const cv::Point2f &y, const int r) { return norm(x - y) < r ? 1 : 0; }
 
 /*
  * Paper's Eq(3)
  */
-float ETF::computeWm(float gradmag_x, float gradmag_y)
+float ETF::computeWm(const float gradmag_x, const float gradmag_y)
 {
-    float wm = (1 + tanh(gradmag_y - gradmag_x)) / 2;
+    const float wm = (1 + tanh(gradmag_y - gradmag_x)) / 2;
     return wm;
 }
 
 /*
  * Paper's Eq(4)
  */
-float ETF::computeWd(cv::Vec3f x, cv::Vec3f y) { return abs(x.dot(y)); }
-
-void ETF::rotateFlow(cv::Mat &src, cv::Mat &dst, float theta)
-{
-    theta = theta / 180.0 * M_PI;
-
-    for (int i = 0; i < src.rows; i++)
-    {
-        for (int j = 0; j < src.cols; j++)
-        {
-            cv::Vec3f v             = src.at<cv::Vec3f>(i, j);
-            float rx                = v[0] * cos(theta) - v[1] * sin(theta);
-            float ry                = v[1] * cos(theta) + v[0] * sin(theta);
-            dst.at<cv::Vec3f>(i, j) = cv::Vec3f(rx, ry, 0.0);
-        }
-    }
-}
-
-void ETF::resizeMat(cv::Size s)
-{
-    cv::resize(flowField, flowField, s, 0, 0, CV_INTER_LINEAR);
-    cv::resize(refinedETF, refinedETF, s, 0, 0, CV_INTER_LINEAR);
-    cv::resize(gradientMag, gradientMag, s, 0, 0, CV_INTER_LINEAR);
-}
+float ETF::computeWd(const cv::Vec3f &x, const cv::Vec3f &y) { return abs(x.dot(y)); }
