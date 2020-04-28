97a9bb9c4a6b5a80359cf4f5a3b8498bab9c0eb6
SSARCandy <ssarcandy@gmail.com>
2020-04-29 01:22:10 +0800

    introduce cpp17

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 rm -rf .coverage coverage.info
 
-./build/cld --src ./data/fingerprint.jpg -o output.jpg --ETF_iter 3 --CLD_iter 8 --debug_img
+time ./build/cld --src ./data/fingerprint.jpg -o output.jpg --ETF_iter 3 --CLD_iter 8 --debug_img
 echo '10459bb86077235db3f85fe64d25f2d8 output.jpg' | md5sum -c - 
 echo '3ea95ae0f6f92478af7fbf97e96b2763 anti-alias.jpg' | md5sum -c - 
 echo '1a68133901751c29efe4febec29d014e visualize-etf.jpg' | md5sum -c - 
@@ -20,3 +20,6 @@ lcov \
 lcov --remove coverage.info '/usr/*' -o coverage.info > /dev/null
 
 genhtml -o .coverage coverage.info
+
+# clean up
+rm *.jpg coverage.info
