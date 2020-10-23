162c08c31cd3cf4f4dfbd622872b54f0cae1aa69
Andy Hsu <ssarcandy@gmail.com>
2020-10-24 07:07:06 +0800

    pref: save one mul

--- src/CLD.cpp
+++ src/CLD.cpp
@@ -7,7 +7,7 @@
 // Eq.(8)
 inline double gauss(double x, double mean, double sigma)
 {
-    return (exp((-(x - mean) * (x - mean)) / (2 * sigma * sigma)) / sqrt(constant::PI * 2.0 * sigma * sigma));
+    return (exp((-(x - mean) * (x - mean)) / (2 * sigma * sigma)) / (sqrt(constant::PI * 2.0) * sigma));
 }
 
 void MakeGaussianVector(double sigma, std::vector<double> &GAU)
